cdbc6eeb307e9d233ee686c18b6c7e437a2f334c
cris lombardo <bigc4997@gmail.com>
2023-07-30 09:03:28 -0400

    get api key

--- src/utils/fetch.tsx
+++ src/utils/fetch.tsx
@@ -6,14 +6,15 @@ export const fetchAPI = async (endpoint: string, parkId: string) => {
         // If so, return data
         return JSON.parse(sessionData)[parkId];
     }
-    
+
+    console.log('key', import.meta.env.VITE_NPS_API_KEY);
     // Make fetch call
     const response = await fetch(`https://developer.nps.gov/api/v1/${endpoint}/?parkCode=${parkId}&api_key=${import.meta.env.VITE_NPS_API_KEY}`);
     const { data }= await response.json();
-    
+
     // Store in session storage
-    sessionStorage.setItem(endpoint, JSON.stringify({parkId: data}));   
-    
+    sessionStorage.setItem(endpoint, JSON.stringify({parkId: data}));
+
     // Return data
     return data;
 }
